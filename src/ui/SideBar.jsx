78fff99e57b4d30f3b284a48eb43d158206aa1d1
Adel <adeleslami0077@yahoo.com>
2025-05-18 19:34:36 +0330

    create CustomNavLink component

--- src/ui/SideBar.jsx
+++ src/ui/SideBar.jsx
@@ -1,3 +1,4 @@
+import { Children } from "react";
 import { HiCollection, HiHome } from "react-icons/hi";
 import { NavLink } from "react-router-dom";
 
@@ -6,20 +7,37 @@ function SideBar() {
     <div className="bg-secondary-0 row-start-1 row-span-2 border-l border-gray-200 p-4">
       <ul className="flex flex-col gap-y-4">
         <li>
-          <NavLink to="/owner/dashboard" className="flex items-center gap-x-2 hover:bg-primary-100/50 hover:text-primary-900 py-1.5 rounded-lg text-secondary-600 transition-all duration-300">
-            <HiHome/>
+        <CustomNavLink to="/owner/dashboard">
+             <HiHome />
             <span>خانه</span>
-          </NavLink>
+        </CustomNavLink>
         </li>
         <li>
-          <NavLink to="/owner/projects" className="flex items-center gap-x-2 hover:bg-primary-100/50 hover:text-primary-900 py-1.5 rounded-lg text-secondary-600 transition-all duration-300">
+          <CustomNavLink to="/owner/projects">
             <HiCollection />
             <span>پروژه ها</span>
-          </NavLink>
+          </CustomNavLink>
         </li>
       </ul>
     </div>
-  ); 
+  );
 }
 
 export default SideBar;
+
+function CustomNavLink({ children, to }) {
+  const navLinkClass =
+    "flex items-center gap-x-2 hover:bg-primary-100/50 hover:text-primary-900 px-2 py-1.5 rounded-lg  transition-all duration-300";
+  return (
+    <NavLink
+      to={to}
+      className={({ isActive }) =>
+        isActive
+          ? `${navLinkClass} bg-primary-100/50 text-primary-900`
+          : `${navLinkClass} text-secondary-600`
+      }
+    >
+      {children}
+    </NavLink>
+  );
+}
