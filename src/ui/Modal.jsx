e9d776cf304bd344cc83bfa0edd8a73951b2575c
Adel <adeleslami0077@yahoo.com>
2025-05-28 18:26:51 +0330

    confirm delete

--- src/ui/Modal.jsx
+++ src/ui/Modal.jsx
@@ -8,7 +8,7 @@ function Modal({ open,title,onClose, children }) {
         <div
           className="fixed top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 
         rounded-lg bg-secondary-0 shadow-lg p-4 transition-all duration-500 ease-out
-        w-64 "
+        w-[calc(100vw-2rem)] md:max-w-lg max-h-[calc(100vh-2rem) overflow-y-auto] "
         >
           <div className="flex items-center justify-between border-b border-secondary-300 pb-2 mb-6">
             <p className="text-secondary-700 font-bold text-base ">
